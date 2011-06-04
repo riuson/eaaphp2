df82e46c46d5f14a8cb3426b5726c0abae3c7484
Vladimir <riuson@gmail.com>
2011-06-04 21:22:28 +0600

    Page prototype.

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+		<title>Avalon Guards's EA Site</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel='stylesheet' type='text/css' href='ea.css'>
+	</head>
+	<body>
+		<div id="main">
+		<div id="header" class="header"><h1 align="center">Avalon Guards's EA Site</h1></div>
+		<div id="menu" class="left_col"><p align="center">Меню</p></div>
+		<div id="content" class="right_col">
+			<h2>Информация</h2>
+			<p>Проект для получения и просмотра данных с сервера EVE API</p>
+
+			<p>Использованная информация:</p>
+			<ul>
+				<li><a href='http://myeve.eve-online.com/api/doc/default.asp'>EVE API Documentation Index</a></li>
+				<li><a href='http://wiki.eve-id.net/'>EVE-Development Network</a></li>
+				<li><a href='http://bughunters.addix.net/igbtest/IGB-commands.html'>EVE Ingame Webbrowser preliminary documentation</a></li>
+			</ul>
+			<p>Предыдущая версия программного обеспечения была написана на C# .Net, с базой данных MS Access.
+			Программа с исходными кодами и файлом БД выложена <a href='http://code.google.com/p/eve-api-accounting/'>здесь</a>.</p>
+		</div>
+		</div>
+	</body>
+</html>
+
