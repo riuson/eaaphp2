a34dc7e23f9de36ecc6e82fdeb2f0d49e2ad4319
Vladimir <riuson@gmail.com>
2011-06-05 11:38:08 +0600

    Styles extracted to css files.

--- index.php
+++ index.php
@@ -2,25 +2,31 @@
 	<head>
 		<title>Avalon Guards's EA Site</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<link rel='stylesheet' type='text/css' href='ea.css'>
+		<link media="screen" rel="stylesheet" href="styles/ea.css" type="text/css">
 	</head>
 	<body>
 		<div id="main">
-		<div id="header" class="header"><h1 align="center">Avalon Guards's EA Site</h1></div>
-		<div id="menu" class="left_col"><p align="center">Меню</p></div>
-		<div id="content" class="right_col">
-			<h2>Информация</h2>
-			<p>Проект для получения и просмотра данных с сервера EVE API</p>
-
-			<p>Использованная информация:</p>
-			<ul>
-				<li><a href='http://myeve.eve-online.com/api/doc/default.asp'>EVE API Documentation Index</a></li>
-				<li><a href='http://wiki.eve-id.net/'>EVE-Development Network</a></li>
-				<li><a href='http://bughunters.addix.net/igbtest/IGB-commands.html'>EVE Ingame Webbrowser preliminary documentation</a></li>
-			</ul>
-			<p>Предыдущая версия программного обеспечения была написана на C# .Net, с базой данных MS Access.
-			Программа с исходными кодами и файлом БД выложена <a href='http://code.google.com/p/eve-api-accounting/'>здесь</a>.</p>
-		</div>
+			<div id="header" class="header"><p>Avalon Guards's EA Site</p></div>
+			<div id="menu" class="menu">
+				<p>Menu</p>
+				<ul>
+					<li class="disabled">Visitors</li>
+					<li><a href="index.php?mode=Api_Errors">API Errors</a></li>
+					<li><a href="index.php?mode=Api_FacWarTopStats">Fractional wars</a></li>
+					<li><a href="index.php?mode=Api_Conversion">Names/Id conversion</a></li>
+				</ul>
+			</div>
+			<div id="content" class="content">
+				<p class="content_header">About</p>
+				<p>Used documentation:</p>
+				<ul>
+					<li><a href='http://myeve.eve-online.com/api/doc/default.asp'>EVE API Documentation Index</a></li>
+					<li><a href='http://wiki.eve-id.net/'>EVE-Development Network</a></li>
+					<li><a href='http://bughunters.addix.net/igbtest/IGB-commands.html'>EVE Ingame Webbrowser preliminary documentation</a></li>
+					<li><a href='http://htmlbook.ru/'>HTML/CSS handbook</a></li>
+				</ul>
+			</div>
+			<div id="footer" class="footer">&copy; 2011 <a href="mailto:riuson(a)gmail(dot)com">riuson</a></div>
 		</div>
 	</body>
 </html>
