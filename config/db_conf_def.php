0a460fc662f9d1ea262f399056da6294a87b0319
Vladimir <riuson@gmail.com>
2011-06-07 23:36:33 +0600

    Added database config file.

---
+++ config/db_conf_def.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Default settings.
+ * If db_conf.php exists, include it to redefine variables.
+ */
+$db_name = "eaaphp2db";
+$db_host = "localhost";
+$db_user = "eauser";
+$db_password = "RSN6s5nLHQhG5DSA";
+?>
