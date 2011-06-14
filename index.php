7e2de1250b93d846503dd83fca29388e2a0acc5f
Vladimir <riuson@gmail.com>
2011-06-14 18:05:41 +0600

    Class Page removed, scripts moved to index.php

--- index.php
+++ index.php
@@ -1,32 +1,109 @@
-<?php
-
-include_once 'classes/page.php';
-$template = "<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 	<head>
-		<title>#title#</title>
-		<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">
-		<meta http-equiv=\"Content-Language\" content=\"en\">
-		<meta name=\"GENERATOR\" content=\"NetBeans 6.9\">
-		<link media=\"screen\" rel=\"stylesheet\" href=\"styles/ea.css\">
-		<link rel=\"shortcut icon\" href=\"favicon.ico\">
-		<script type=\"text/javascript\" src=\"scripts/jquery-1.6.1.min.js\"></script>
-		<script type=\"text/javascript\" src=\"scripts/jquery.dataTables.js\"></script>
+		<title>Avalon Guards's EA Site</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta http-equiv="Content-Language" content="en">
+		<meta name="GENERATOR" content="NetBeans 6.9">
+		<link media="screen" rel="stylesheet" href="styles/ea.css">
+		<link rel="shortcut icon" href="favicon.ico">
+		<script type="text/javascript" src="scripts/jquery-1.6.1.min.js"></script>
+		<script type="text/javascript" src="scripts/jquery.dataTables.js"></script>
 	</head>
 	<body>
-		<div id=\"main\">
-			<div id=\"header\"  class=\"header\">#title#</div>
-			<div id=\"status\"  class=\"status\">#status#</div>
-			<div id=\"menu\"    class=\"menu\">#menu#</div>
-			<div id=\"content\" class=\"content\">#content#</div>
-			<div id=\"footer\"  class=\"footer\">
-				&copy; 2011 <a href=\"mailto:riuson(a)gmail(dot)com\">riuson</a>
+		<div id="main">
+			<div id="header"  class="header">Avalon Guards's EA Site</div>
+			<div id="status"  class="status">#status#</div>
+			<div id="menu"    class="menu">#menu#</div>
+			<div id="content" class="content">#content#</div>
+			<div id="footer"  class="footer">
+				&copy; 2011 <a href="mailto:riuson(a)gmail(dot)com">riuson</a>
 			</div>
 		</div>
-		<script>#script#</script>
-	</body>
-</html>";
+		<script type="text/javascript" >
+			function updateStatus()
+{
+	$.ajax({
+		type: "POST",
+		url: "backend.php",
+		cache: false,
+		data: "call=sys_status",
+		success: function(html) {
+			$("#status").html(html);
+			bindStatus();
+		}
+	});
+	return false;
+}
+function updateMenu()
+{
+	$.ajax({
+		type: "POST",
+		url: "backend.php",
+		cache: false,
+		data: "call=sys_menu&item=" + $(this).attr('href'),
+		success: function(html) {
+			$("#menu").html(html);
+			$("#menu a").bind("click", updateMenu);
+		}
+	});
+	loadContent($(this).attr('href'));
+	return false;
+}
+function updateMenuDefault()
+{
+	$.ajax({
+		type: "POST",
+		url: "backend.php",
+		cache: false,
+		data: "call=sys_menu",
+		success: function(html) {
+			$("#menu").html(html);
+			$("#menu a").bind("click", updateMenu);
+		}
+	});
+	return false;
+}
+function loadContent(modeName)
+{
+	var aData = {
+		call: modeName
+	}
+	loadContentWithData(aData);
+	return false;
+}
+function loadContentWithData(aData)
+{
+	$.ajax({
+		type: "POST",
+		url: "backend.php",
+		cache: false,
+		data: aData,
+		success: function(html) {
+			$("#content").html(html);
+			bindContent();
+			updateStatus();
+		}
+	});
+	return false;
+}
 
-$p = new Page();
-$p->writeAll($template);
-?>
+$(document).ready(function() {
+
+	loadContent('mode_about');
+
+	$.ajax({
+		type: "POST",
+		url: "backend.php",
+		cache: false,
+		data: "call=sys_menu&item=mode_about",
+		success: function(html) {
+			$("#menu").html(html);
+			$("#menu a").bind("click", updateMenu);
+		}
+	});
+
+});
+		</script>
+	</body>
+</html>
